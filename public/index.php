aea97898e64ae6f565663a973eab0a1c78464b1a
Ronaldo Ortiz <ronyortizop@yahoo.com>
2017-09-12 09:51:57 -0400

    Initial Commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Globe Bank</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1>Globe Bank: coming soon...</h1>
+
+</body>
+</html>
---
+++ public/staff/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>BGI</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" media="all" href="../css/style.css">
+</head>
+<body>
+  <header>
+    <h1>GBI Staff Area</h1>
+  </header>
+
+  <nav>
+    <ul>
+      <li><a href="index.php">Menu</a></li>
+    </ul>
+  </nav>
+
+  <footer>
+    &copy; <?php echo date('Y'); ?> Globe Bank
+  </footer>
+
+</body>
+</html>
+
